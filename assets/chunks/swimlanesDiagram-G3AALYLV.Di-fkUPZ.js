57eb2750d4ddec40e10e995798c83d7d52241184
navneetkumar-pim-webkul <navneetkumar-pim-webkul@users.noreply.github.com>
2026-07-03 12:07:15 +0000

    deploy: 90b1c0014f15463316afa0d8990da6d99830e983

---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.Di-fkUPZ.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.B1GZbP-O.js";import{_ as a}from"../app.DE2_MbEN.js";import"./chunk-5VM5RSS4.BXadokY6.js";import"./chunk-XXDRQBXY.B6BOZzay.js";import"./chunk-VR4S4FIN.D_WIXIdz.js";import"./chunk-32BRIVSS.B8IdvxJp.js";import"./channel.Dxe9iPik.js";import"./framework.-FZumj2l.js";import"./theme.CptaaoLa.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
