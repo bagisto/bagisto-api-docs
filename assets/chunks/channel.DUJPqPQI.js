e3ea09992238e7a5990656a1d49a19dc650b9d35
navneetkumar-pim-webkul <navneetkumar-pim-webkul@users.noreply.github.com>
2026-08-26 06:27:52 +0000

    deploy: 2a87493f826dc0648ca87f879758e665d76e4725

--- assets/chunks/channel.DUJPqPQI.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"../app.eGz0Llsm.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.YTv5hrLj.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.DHMvxfJ3.js";import{_ as a}from"../app.CASSX5AF.js";import"./chunk-5VM5RSS4.C3kgMaTH.js";import"./chunk-XXDRQBXY.BJYFDvS_.js";import"./chunk-VR4S4FIN.Y5olAc41.js";import"./chunk-32BRIVSS.BERVNRYt.js";import"./channel.BROWXCZd.js";import"./framework.CJDy2q7X.js";import"./theme.D0553hjA.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
